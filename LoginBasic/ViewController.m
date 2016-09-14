a462c60bc0154eaf5e4399015a636a882d2584e7
patricknasc <patrick.nascimento@gmail.com>
2016-09-14 20:40:53 -0300

    Login Application - Tutorial for practicing outlets, actions and some basics of ObjC https://www.youtube.com/watch?v=z-X0ZvTOJ7Q

--- LoginBasic/ViewController.m
+++ LoginBasic/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSString *username;
+@property (nonatomic, strong) NSString *password;
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UILabel *notificationLabel;
+
 @end
 
 @implementation ViewController
@@ -17,6 +23,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.username = @"patricknasc";
+    self.password = @"12345";
+    self.passwordTextField.secureTextEntry = YES;
+    
+    
+    NSLog(@"%@",self.username);
 }
 
 
@@ -25,5 +38,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)loginWasPressed:(id)sender {
+    
+    BOOL isUsernameEquals = [self.username isEqualToString: [self.userNameTextField text]];
+    BOOL isPasswordEquals = [self.password isEqualToString: [self.passwordTextField text]];
+    
+    if(isUsernameEquals && isPasswordEquals){
+        NSLog(@"SUCCESS!");
+        [self.notificationLabel setText:@"Congratulations you have logged in!"];
+    } else {
+        NSLog(@"FAILURE!");
+        [self.notificationLabel setText:@"Your username or password was incorrect!"];
+    }
+}
 
 @end
